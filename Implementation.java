4451284978344f38e346fbcd04af15d52e7ee1dc
whong4 <44105216+whong4@users.noreply.github.com>
2018-10-13 15:17:26 -0400

    Update Implementation.java

--- Implementation.java
+++ Implementation.java
@@ -3,13 +3,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JFileChooser;
 public class Implementation {
    public static void main(String[] args) { 
-      final int MAX_STUDENTS = 30000;
+      final int MAX_STUDENTS = 100;
+      
+      Student[] studentOptions = new Student[MAX_STUDENTS];
+      int studentCounter = Student.getNumberOfStudents();
     
       int studentOption = getStudentOptions();
       while(studentOption != 2) {
          switch(studentOption) {
             case 1: 
-               addStudent();
+               addStudent(studentOptions);
                break;
                 
             case 2: 
